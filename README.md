f70737ae30093966ed73178f836f90c08fa3c884
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-11-28 13:05:55 -0500

    Updates

---
+++ README.md
@@ -0,0 +1,126 @@
+# Memory Game Project: Version Two
+
+## Introduction
+
+As the second project in Udacity's Front-End Nanodegree program, the commits made on this repo will demonstrate my abilities with using HTML, CSS, and JavaScript to build a fully-functioning web-based game.
+
+The below game description and project specification are based on Udacity's own guidelines for completing the project.
+
+## How The Game Works
+
+The game board is made up of 16 cards appearing as a grid. Each of the cards will feature one of eight different symbols (likely a CSS icon) and cards are meant to be matched according to the symbols. The game begins with all cards presented symbol-side face down. The gameplay turn rules are just like the classic game where you pick one card from the board to reveal its symbol and then try to find its match from the other cards remaining unmatched on the grid.
+
+Each turn:
+
+- The player flips one card over to reveal its underlying symbol.
+- The player then turns over a second card, trying to find the corresponding card with the same symbol.
+- If the cards match, both cards stay flipped over.
+- If the cards do not match, both cards are flipped face down.
+
+The game ends once all cards have been correctly matched.
+
+## Viewing The Project Online
+
+You can view a deployed version of my Memory Game application at the following address:
+https://github.com/Jamoverjelly/Udacity-project-2-Memory-Game
+
+<hr>
+
+## Project Specification
+
+#### Game Behavior
+
+##### Memory Game Logic
+
+The game randomly shuffles the cards. A user wins once all cards have successfully been matched.
+
+##### Congratulations Popup
+
+When a user wins the game, a modal appears to congratulate the player and ask if they want to play again. It should also tell the user how much time it took to win the game, and what the star rating was.
+
+##### Restart Button
+
+A restart button allows the player to reset the game board, the timer, and the star rating.
+
+##### Star Rating
+
+The game displays a star rating (from 1 to at least 3) that reflects the player's performance. At the beginning of a game, it should display at least 3 stars. After some number of moves, it should change to a lower star rating. After a few more moves, it should change to a even lower star rating (down to 1).
+
+The number of moves needed to change the rating is up to you, but it should happen at _some_ point.
+
+##### Timer
+
+When the player starts a game, a displayed timer should also start. Once the player wins the game, the timer stops.
+
+##### Move Counter
+
+Game displays the current number of moves a user has made.
+
+<hr>
+<br>
+
+#### Interface Design
+
+##### Styling
+
+Application uses CSS to style components for the game.
+
+##### Usability
+
+All application components are usable across modern desktop, tablet, and phone browsers.
+
+<hr>
+<br>
+
+#### Documentation
+
+##### README
+
+A `README` file is included detailing the game and all dependencies.
+
+##### Comments
+
+Comments are present and effectively explain longer code procedure when necessary.
+
+##### Code Quality
+
+Code is formatted with consistent, logical, and easy-to-read formatting as described in the [Udacity JavaScript Style Guide](http://udacity.github.io/frontend-nanodegree-styleguide/javascript.html).
+
+<hr>
+<br>
+
+## Required Dependencies
+
+- [Font Awesome](https://fontawesome.com/) - for incorporating specialized vector icons in the game's cards and UI
+
+- [Google Fonts](https://fonts.google.com/) - for applying open source typographical font styling to the game's text content
+
+## Attribution References
+
+- Helper code for building the modal was referenced on CSS-Tricks
+  at https://css-tricks.com/considerations-styling-modal/
+  on July 2, 2018
+
+- Helper code for building the modal overlay was referenced from
+  Codrops article, CSS Overlay Techniques, by Sara Soueidan
+  at https://css-tricks.com/considerations-styling-modal/
+  on July 2, 2018
+
+- Helper code for styling buttons of the modal was referenced on CSS-Tricks
+  at https://css-tricks.com/overriding-default-button-styles/
+  on July 2, 2018
+
+- Helper code for updating the page's score counter was referenced at
+  https://stackoverflow.com/questions/15280851/javascript-increment-value-inside-html
+  on June 29, 2018
+
+- Referenced helper code for updating the timer, and
+  starting and stopping the timer from Udacity Student Leader,
+  Chris N's post on the Slack Channel for this project (fend_proj_2).
+  The full post can be found at this link:
+  https://gwgnanodegrees.slack.com/files/UA8PXHUR3/FB0Q3CSMB/Getting_the_Memory_Game_timer_to_work
+  and was accessed on June 30, 2018.
+
+- Original code for _version one_ of this project was built following
+  Matthew Cranford's Memory Game Walkthrough which was accessed on June 21, 2018
+  from: https://matthewcranford.com/memory-game-walkthrough-part-1-setup/
---
+++ js/app.js
@@ -0,0 +1,337 @@
+// Get parent element, deck and store into global variable reference
+const deck = document.querySelector('.deck');
+
+// call function to shuffle cards each time page is loaded. NOTE, global variable, deck must be declared first
+shuffleCards();
+
+// Create global, mutable array for storing up to two cards for checking
+let openCards = [];
+
+// Create mutable variable for storing player move count
+let playerMoveCount = 0;
+
+// Create mutable variable for storing number of stars in global namespace
+let starCount = 0;
+
+// Initialize mutable variables for working with the timer functionality
+let timerSeconds = 0;
+let timerMinutes = 0;
+let timer;
+let timerActive = false;
+
+// Set mutable counter variable for storing matches in global namespace
+let matchCount = 0;
+
+// Create event listener to restart the game, when the restart button is clicked
+document.querySelector('.restart').addEventListener('click', resetGame);
+
+// Declare variables for getting and working with interactive modal elements
+// from document
+const closeIcon = document.querySelector('#close');
+
+const cancelButton = document.querySelector('.cancel-btn');
+
+const replayButton = document.querySelector('.replay-btn');
+
+// Event listener delegated to parent, listening for clicks on all children
+deck.addEventListener('click', function(e) {
+    // card variable initialized in scope references the target property of the click event
+    const card = e.target;
+    // First validate context of click event before running additional functionality
+    if (clickCheck(card)) {
+        startTimer();
+        toggleClass(card);
+        openCards.push(card);
+        if (openCards.length === 2) {
+            checkForMatch();
+            countMove();
+            checkMovesCount();
+        }
+    }
+});
+
+// Function checking and limiting the click event to set of unmatched, unique cards:
+function clickCheck(card) {
+    return (
+        // Confirm the length of cards array is less than 2
+        openCards.length < 2 &&
+        // Confirm target of click event does not already exist in the array
+        !openCards.includes(card) &&
+        // Confirm the click occured on element having a class of 'card'
+        card.classList.contains('card') &&
+        // Confirm the click did not occur on card already having class of match (discount this move)
+        !card.classList.contains('match')
+    );
+}
+
+// Toggle open and show class on card to reveal uncovered side
+function toggleClass(card) {
+    card.classList.toggle('open');
+    card.classList.toggle('show');
+}
+
+function checkForMatch() {
+    // check for match
+    if (openCards[0].firstElementChild.className == 
+        openCards[1].firstElementChild.className) {
+            saveMatch();
+            countMatches();
+    } else {
+            misMatch();
+        }
+}
+
+function saveMatch() {
+    openCards[0].classList.toggle('match');
+    openCards[1].classList.toggle('match');
+    openCards = [];
+    matchCount++;
+}
+
+function countMatches() {
+    // Set winning condition number
+    const winCount = 8;
+    // If number matched cards equals winning condition, run functions to end the game
+    if(matchCount === winCount) {
+        setTimeout(function() {
+            updateModal();
+            stopTimer();
+            showModal();
+            starCount = 0;
+            matchCount = 0;
+        }, 1000);
+    }
+}
+
+function misMatch() {
+    setTimeout(function () {
+        for (card of openCards) {
+            card.classList.toggle('open');
+            card.classList.toggle('show');
+        }
+        openCards = [];
+    }, 1000);
+}
+
+// ######## Start Reference to Helper Code ######## //
+
+// Set player's number of moves equal to value of playerMoveCount
+function countMove() {
+    // Increment move score value after each validated click
+    playerMoveCount++;
+    // Get player moves element from document
+    const playerMoveBoard = document.querySelector('.moves');
+    // Update page text using innerHTML
+    playerMoveBoard.innerHTML = playerMoveCount;
+}
+// ######## End Reference for Helper Code ######## //
+
+/* Begin Attribution
+    Referenced helper code for updating the page's score counter at
+    https://stackoverflow.com/questions/15280851/javascript-increment-value-inside-html
+    on June 29, 2018
+End Attribution */
+
+function checkMovesCount() {
+    // set terms for removing star penalty
+    if (playerMoveCount === 16 || playerMoveCount === 24) {
+        hideStar();
+    }
+}
+
+function hideStar() {
+    // get the star elements from the document object
+    const starsSelect = document.querySelectorAll('.stars li');
+    // hide star from page
+    for (star of starsSelect) {
+        // if element in starsSelect collection, does not have its display class set to none, set it to none
+        if (star.style.display !== 'none') {
+            star.style.display = 'none';
+            // Add break to avoid removing more than single star at a time.
+            break;
+        }
+    }
+}
+
+// ######## Start Reference to Helper Code ######## //
+
+function updateTimer() {
+    timerSeconds++;
+
+    if (timerSeconds < 10) {
+        timerSeconds = `0${timerSeconds}`
+    }
+
+    if (timerSeconds >= 60) {
+        timerMinutes++;
+        timerSeconds = '00';
+    }
+
+    const pageTimer = document.querySelector('.timer')
+    
+    pageTimer.innerHTML = timerMinutes + ':' + timerSeconds;
+}
+
+function startTimer() {
+    if (timerActive == false) {
+        timer = setInterval(updateTimer, 1000);
+        timerActive = true;
+    }
+}
+
+function stopTimer() {
+    clearInterval(timer);
+    timerSeconds = 0;
+    timerMinutes = 0;
+    timerActive = false;
+}
+
+// ######## End Reference for Helper Code ######## //
+
+// ######## Begin Attribution ######## //
+// Referenced helper code for updating the timer, and
+// starting and stopping the timer from Udacity Student Leader,
+// Chris N's post on the Slack Channel for this project (fend_proj_2).
+// The full post can be found at this link: 
+// https://gwgnanodegrees.slack.com/files/UA8PXHUR3/FB0Q3CSMB/Getting_the_Memory_Game_timer_to_work
+// and was accessed on June 30, 2018.
+
+// ######## End Attribution ######## //
+
+function shuffleDeck() {
+    const cardsToShuffle = Array.from(document.querySelectorAll(".deck li"));
+    const shuffledCards = shuffle(cardsToShuffle);
+    for (card of shuffledCards) {
+        deck.appendChild(card);
+        card.classList.value = "card";
+    }
+}
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function shuffleCards() {
+    // Get children of deck, cards and store into variable reference
+    const cards = document.querySelectorAll('.card');
+   
+    // Array created from NodeList
+    const cardsArray = Array.from(cards);
+   
+    // Pass the cardsArray object as parameter to shuffle function and assign to new variable
+    const shuffledCards = shuffle(cardsArray);
+   
+    // Deliver each card object to the parent 'deck' global in the new shuffled order
+    shuffledCards.forEach(function(card) {
+       // Card objects are successively updated into DOM parent node object
+       deck.appendChild(card);
+   });
+}
+
+function resetCards() {
+    const cards = document.querySelectorAll('.card');
+    for (card of cards) {
+        if (card.classList.contains('show') ||
+            card.classList.contains('show') ||
+            card.classList.contains('match')
+        ) {
+            card.className = 'card';
+        }
+    }
+}
+
+function resetMoveCount() {
+    playerMoveCount = 0;
+    // Get player moves element from document
+    const playerMoveBoard = document.querySelector('.moves');
+    // Update page text using innerHTML
+    playerMoveBoard.innerHTML = playerMoveCount;
+}
+
+function resetTimer() {
+    stopTimer();
+    const pageTimer = document.querySelector('.timer');
+    pageTimer.innerHTML = timerMinutes + ':' + `0${timerSeconds}`;
+}
+
+function resetStars() {
+    // get the star elements from the document object
+    const starsSelect = document.querySelectorAll('.stars li');
+    // reveal all hidden stars
+    for (star of starsSelect) {
+        // if element in starsSelect collection, does not have its display class set to none, set it to none
+        if (star.style.display == 'none') {
+            star.style.display = 'inline-block';
+        }
+    }
+    // reset count of stars in starCount variable
+    starCount = 0;
+}
+
+function getStarCount() {
+    const starsSelect = document.querySelectorAll('.stars li');
+    for (star of starsSelect) {
+        if(star.style.display !== 'none') {
+            starCount++;
+        }
+    }
+    return starCount;
+}
+
+function updateModal() {
+    // get time-stat from modal
+    const pageTimeStat = document.querySelector('.time-stat');
+    // write time-stat to modal
+    pageTimeStat.innerHTML = 'Time: ' + timerMinutes + ':' + timerSeconds;
+
+    // get moves-stat from modal
+    const pageMovesStat = document.querySelector('.moves-stat');
+    // write moves-stat to modal
+    pageMovesStat.innerHTML = 'Moves: ' + `${playerMoveCount}`
+
+    // get star-stat from modal
+    const pageStarStat = document.querySelector('.star-stat');
+    // call getStarCount to get starCount value
+    getStarCount();
+    // write star-stat to modal
+    pageStarStat.innerHTML = 'Stars: ' + `${starCount}`
+}
+
+function showModal() {
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.toggle('hide');
+}
+
+closeIcon.addEventListener('click', function() {
+    showModal();
+});
+
+cancelButton.addEventListener('click', function() {
+    showModal();
+});
+
+replayButton.addEventListener('click', function() {
+    showModal();
+    resetGame();
+});
+
+function resetGame() {
+    resetTimer();
+    resetStars();
+    shuffleCards();
+    resetMoveCount();
+    resetCards();
+    matchCount = 0;
+    openCards = [];
+}
